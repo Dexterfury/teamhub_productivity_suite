3f1f274fc624b4fbf51f7fe25257c3a3f02b8b49
Dexterfury <raphaelsquared@gmail.com>
2025-05-27 21:20:58 +0200

    feat: integrate Firebase into the project with configuration files and initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:teamhub_productivity_suite/firebase_options.dart';
 import 'package:teamhub_productivity_suite/src/routing/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
